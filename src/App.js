e61821b04049c6f642039e91b310741456de37ba
mezitta <michaelezitta@gmail.com>
2024-02-17 10:45:01 -0500

    Section setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import Home from './components/Home'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
@@ -10,8 +10,56 @@ const App = () => {
       </header> */}
       <Navbar />
       <Home />
+
+      <div className="container-fluid page-section section-1" id="about">
+        <div className="row section-title"></div>
+        <div className="row section-body">
+          <div className="col">
+            <div className="sub-section-title"></div>
+          </div>
+          <div className="col">
+            <div className="sub-section-title"></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid page-section section-2" id="work">
+        <div className="row section-title"></div>
+        <div className="row section-body">
+          <div className="col">
+            <div className="sub-section-title"></div>
+          </div>
+          <div className="col">
+            
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid page-section section-1" id="projects">
+        <div className="row section-title"></div>
+        <div className="row section-body">
+          <div className="col">
+            <div className="sub-section-title"></div>
+          </div>
+          <div className="col">
+            
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid page-section section-2" id="contact">
+        <div className="row section-title"></div>
+        <div className="row section-body">
+          <div className="col">
+            <div className="sub-section-title"></div>
+          </div>
+          <div className="col">
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,24 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div className="navbar">
-            Navbar
+        <div className="nav">
+            <div className="nav-button-wrapper">
+                <a className="nav-button" href="#about">
+                    About
+                </a>
+
+                <a className="nav-button" href="#work">
+                    Work
+                </a>
+
+                <a className="nav-button" href="#projects">
+                    Projects
+                </a>
+
+                <a className="nav-button" href="#contact">
+                    Contact
+                </a>
+            </div>
         </div>
     )
 }

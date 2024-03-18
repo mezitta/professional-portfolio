33016f9aa638b80868993641adbd0a0c561527d2
mezitta <michaelezitta@gmail.com>
2024-03-18 17:47:22 -0400

    return to home button

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -4,7 +4,12 @@ const PageNotFound = () => {
     return (
         <div className="container-fluid">
             <div className="container pnf-container">
-                <h1>Page not found</h1>
+                <div className="pnf-wrapper">
+                    <h1>Page not found</h1>
+                    <h2>
+                        <a href="/">Back to Site</a>
+                    </h2>
+                </div>
             </div>
         </div>
     )
